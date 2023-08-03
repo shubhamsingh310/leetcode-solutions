5b991c3014fd5df6bf48d47a1da418a0fcb55851
Shubham Singh <shubhamsingh3102002s@gmail.com>
2023-08-03 22:35:50 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (14.74%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -15,7 +15,9 @@ class Solution {
             swap(nums[index], nums[j]);
         }
     }
-public:
+    
+    
+ public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         int index = 0;
