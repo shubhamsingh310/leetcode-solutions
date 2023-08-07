db060e9492421bedd26e515dc075a684c66b1890
Shubham Singh <shubhamsingh3102002s@gmail.com>
2023-08-07 23:47:57 +0530

    Time: 6 ms (33.67%), Space: 9.6 MB (19.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,42 +2,30 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-/*      finding row and col size      */
-        int row = matrix.size(); //3
-        int col = matrix[0].size(); //4
+        int row = matrix.size();
+        int col=matrix[0].size();
         
-        
-/*      init start and end      */        
-        int start = 0; //0
-        int end = row*col - 1; //11
-        
-/*      finding mid index      */         
-        int mid = start + (end-start) / 2; // 6 for first example
-        
-        while(start<=end)
-        {
+        int start = 0;
+        int end = row*col-1;
             
-/*      mid index we find was in linear form and we have to find where mid is in row and col      
-
-to find mid index in row = mid / col i.e 6/4 = 1
-to find mid index in col = mid % col i.e 6%4 = 2
-
-matrix[1][2] = index 6 
-
-*/          int matrixIndex = matrix[mid / col][mid % col];
+        int mid= start + (end-start)/2;
+        
+        while(start<=end) {
             
-            if(matrixIndex == target){ return 1; }
-            
-            if(matrixIndex < target)
-            {
-                start = mid + 1;
-            }else if(matrixIndex > target)
-            {
-                end = mid - 1;
+            int element = matrix[mid/col][mid%col];
+            if(element == target) {
+                return 1;
             }
-            // update mid
-            mid = start + (end-start) / 2;
+            if(element < target) {
+                start=mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+            mid = start + (end-start)/2;
         }
+        
         return 0;
     }
 };
