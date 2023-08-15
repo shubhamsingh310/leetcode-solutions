a4f661f38bf14a6147ed321e31db39fc96d3fe19
Shubham Singh <shubhamsingh3102002s@gmail.com>
2023-08-15 13:49:37 +0530

    Time: 9 ms (22.93%), Space: 8.6 MB (5.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,44 +1,4 @@
 
-// class Solution
-// {
-//     bool isEqual(int l,int r)
-//     {
-//         if(l==r || (l>='A' && l<='Z' && r==l+32)
-//           || (r>='A' && r<='Z' && l==r+32))
-//             return true;
-        
-//         return false;
-//     }
-    
-// public:
-//     bool isPalindrome(string s) {
-//         int r = s.size();
-//         if(r==0)
-//             return true;
-        
-//         vector<int> arr;
-//         int i=0;
-//         while(i<r)
-//         {
-//             if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9'))
-//                 arr.push_back(s[i]);
-//             i+=1;
-//         }
-        
-//         int l=0;
-//         r = arr.size();
-//         while(l<r)
-//         {
-//             if(isEqual(arr[l],arr[r-1])==false)
-//                 return false;
-//             l++;
-//             r--;
-//         }
-//         return true;
-//     }
-// };
-
-
 class Solution
 {
     
@@ -57,7 +17,6 @@ class Solution
             
             if( r == 0)
                 return true;
-            
         
         vector<int>arr;
         int i = 0;
@@ -80,8 +39,6 @@ class Solution
                 r--;
             
         }
-        
             return true;
-        
     }
 };
